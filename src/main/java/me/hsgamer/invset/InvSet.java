c7e978ad9136d21299f0a6401bcc9135ebe96c3a
HSGamer <huynhqtienvtag@gmail.com>
2024-03-02 23:43:36 +0700

    disable checks on NBTAPI

--- src/main/java/me/hsgamer/invset/InvSet.java
+++ src/main/java/me/hsgamer/invset/InvSet.java
@@ -1,10 +1,18 @@
 package me.hsgamer.invset;
 
+import de.tr7zw.changeme.nbtapi.utils.MinecraftVersion;
 import me.hsgamer.hscore.bukkit.command.CommandManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class InvSet extends JavaPlugin {
     private final CommandManager commandManager = new CommandManager(this);
+
+    @Override
+    public void onLoad() {
+        MinecraftVersion.disableBStats();
+        MinecraftVersion.disablePackageWarning();
+        MinecraftVersion.disableUpdateCheck();
+    }
 
     @Override
     public void onEnable() {
